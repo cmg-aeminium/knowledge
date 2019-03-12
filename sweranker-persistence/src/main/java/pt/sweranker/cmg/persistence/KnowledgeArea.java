6388594056996064855d3668429c0c725c08e55a
Carlos Manuel <carlos.manuel@farfetch.com>
2019-03-12 18:51:17 +0000

    Inclusion of first entity, nonfunctioning

---
+++ sweranker-persistence/src/main/java/pt/sweranker/cmg/persistence/KnowledgeArea.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright (c) 2019
+ * 
+ * Carlos Gonçalves (https://www.linkedin.com/in/carlosmogoncalves/)
+ *
+ * All rights reserved.
+ */
+package pt.sweranker.cmg.persistence;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class KnowledgeArea implements Serializable {
+
+    private static final long serialVersionUID = 4341439068096536870L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name", nullable = false, length = 50)
+    private String name;
+
+    @Column(name = "description", nullable = false, length = 2000)
+    private String description;
+
+    @Column(name = "image", nullable = true)
+    private String image;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+}
