f2b329c41d24537816fc442fdf0786698b881434
Carlos Gonçalves <carlosmoliveiragoncalves@gmail.com>
2020-01-19 23:51:03 +0000

    Running in Payara Micro now, yay

---
+++ src/main/java/pt/sweranker/config/datasource/DatasourceProvider.java
@@ -0,0 +1,32 @@
+package pt.sweranker.config.datasource;
+
+import javax.annotation.Resource;
+import javax.annotation.sql.DataSourceDefinition;
+import javax.ejb.Singleton;
+import javax.enterprise.inject.Produces;
+import javax.sql.DataSource;
+
+/**
+ * @author Carlos Gonçalves
+ */
+@Singleton
+@DataSourceDefinition(
+    name = "java:global/SwerankerDataSource",
+    className = "${datasource.javaclass}",
+    user = "${database.user}",
+    password = "${database.password}",
+    serverName = "${database.host}",
+    portNumber = 5445, //port must always be an integer
+    databaseName = "${database.name}",
+    properties = {"stringtype=unspecified", "fish.payara.log-jdbc-calls=true"})
+public class DatasourceProvider {
+
+    @Resource(lookup = "java:global/SwerankerDataSource")
+    private DataSource datasource;
+
+    @Produces
+    public DataSource getDatasource() {
+        return datasource;
+    }
+
+}
