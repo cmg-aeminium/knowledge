0fe23d60760b8fa6b8d56929e176709168914854
Carlos Gonçalves <carlosmoliveiragoncalves@gmail.com>
2020-04-23 17:08:09 +0100

    Some missing tables and 1st try at a dockerfile

--- setup/create-tables.sql
+++ setup/create-tables.sql
@@ -5,3 +5,14 @@ CREATE DATABASE sweranker
     CONNECTION LIMIT = -1;
     
 
+CREATE TABLE KnowledgeAreas (
+    id bigserial NOT NULL PRIMARY KEY,
+	image text NOT NULL
+);
+
+CREATE TABLE KnowledgeAreaTranslations (
+    knowledgeAreaId bigserial NOT NULL REFERENCES KnowledgeAreas(id) ON UPDATE NO ACTION ON DELETE NO ACTION,
+	language text NOT NULL,
+	translatedName text NOT NULL,
+	translatedDescription text NOT NULL
+);
