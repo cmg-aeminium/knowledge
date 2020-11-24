3eea37b25bf209b2b8db3c0b0bb4ae230b0b3bb1
Carlos Gonçalves <carlosmoliveiragoncalves@gmail.com>
2020-11-24 18:14:25 +0000

    Commit of Degree Class

---
+++ src/main/java/pt/sweranker/persistence/entities/degrees/DegreeClass.java
@@ -0,0 +1,32 @@
+/**
+ * Copyright (c) 2020  Carlos Gonçalves (https://www.linkedin.com/in/carlosmogoncalves/)
+ * Likely open-source, so copy at will, bugs will be yours as well.
+ */
+package pt.sweranker.persistence.entities.degrees;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+/**
+ * @author Carlos Gonçalves
+ */
+@Entity
+public class DegreeClass {
+
+    @Id
+    private Long id;
+
+    @Column(name = "year", nullable = false)
+    private Integer year;
+
+    @Column(name = "semester")
+    private Integer semester;
+
+    @Column(name = "ects", nullable = false)
+    private Integer etcs;
+
+    @Column(name = "isoptional", nullable = false)
+    private Boolean isOptional;
+
+}
